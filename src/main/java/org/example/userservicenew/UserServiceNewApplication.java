c19086e5d9d8aa6c6626c1ffdf3ade0fe500b899
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2024-11-07 17:51:05 +0530

    add new dependency

--- src/main/java/org/example/userservicenew/UserServiceNewApplication.java
+++ src/main/java/org/example/userservicenew/UserServiceNewApplication.java
@@ -1,12 +1,18 @@
 package org.example.userservicenew;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {EurekaClientAutoConfiguration.class})
+@EnableEurekaServer
 public class UserServiceNewApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(UserServiceNewApplication.class, args);
     }
 
